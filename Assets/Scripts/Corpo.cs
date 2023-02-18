01877f40e9064c182dc82f84cd317429af5f5b19
jessicaduque <69450467+jessicaduque@users.noreply.github.com>
2023-02-17 21:01:52 -0300

    Aula de hoje / correr, arama, rotação

--- Assets/Scripts/Corpo.cs
+++ Assets/Scripts/Corpo.cs
@@ -6,11 +6,15 @@ public class Corpo : MonoBehaviour
 {
 
     private Rigidbody Rb;
+    private Animator Anim;
+    public float sensibilidade;
+    private float velocidadeP;
 
     // Start is called before the first frame update
     void Start()
     {
         Rb = GetComponent<Rigidbody>();
+        Anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,8 +25,34 @@ public class Corpo : MonoBehaviour
 
     void Mover()
     {
-        float velZ = Input.GetAxis("Vertical");
-        float velX = Input.GetAxis("Horizontal");
-        Rb.velocity = new Vector3(velX, Rb.velocity.y, velZ);
+        // Mover
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            velocidadeP = 12;
+        }
+        else
+        {
+            velocidadeP = 5;
+        }
+
+
+        float velZ = Input.GetAxis("Vertical") * velocidadeP;
+        float velX = Input.GetAxis("Horizontal") * velocidadeP;
+        // PosCorrigida
+        Vector3 velCorrigida = velX * transform.right + velZ * transform.forward;
+        Rb.velocity = new Vector3(velCorrigida.x, Rb.velocity.y, velCorrigida.z);
+
+        // Animação de andar do personagem
+        if (velX != 0 || velZ != 0)
+        {
+            Anim.SetBool("Andar", true);
+        }
+        else if(velX == 0 && velZ == 0){
+            Anim.SetBool("Andar", false);
+        }
+
+        // Movimento girar do player
+        float mouseX = Input.GetAxis("Mouse X") * sensibilidade * Time.deltaTime;
+        transform.Rotate(Vector3.up * mouseX);
     }
 }
